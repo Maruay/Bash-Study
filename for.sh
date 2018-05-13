532e4eeba57f7afb5e14d40ea7ac1abe6cd118ee
Maruay <maruay.wk@gmail.com>
2018-05-13 11:53:37 +0700

    check  &  not null

--- for.sh
+++ for.sh
@@ -2,6 +2,9 @@
 # specific conversion script for extension x to y
 
 #LIST="$(ls *.$1)"
+if [ -z "$1" ] || [ -z "$2" ]; then
+echo "Please fill both file's extension"
+else
 for file in *.$1; do
 NEWNAME="$(basename $file .$1).$2"
 cp $file $NEWNAME
@@ -12,3 +15,4 @@ cp $file $NEWNAME
 #cat "$i" | sed -e '1,23d' | tac | sed -e '1,21d'| tac >> "$NEWNAME"
 #cat endfile >> "$NEWNAME"
 done
+fi
