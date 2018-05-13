a6a931688f891a7e645b7b2e3f45af643e8f8fe0
Maruay <maruay.wk@gmail.com>
2018-05-13 10:33:06 +0700

    modified for.sh

--- for.sh
+++ for.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # specific conversion script for extension x to y
 
-LIST="$(ls *.$1)"
-for i in "$LIST"; do
-echo "$(basename "$i" .$1).$2"
+#LIST="$(ls *.$1)"
+for file in *.$1; do
+NEWNAME="$(basename $file .$1).$2"
+cp $file $NEWNAME
 # echo "$(basename "$i" .$1).$2"
 # echo "${i/%.$1/.$2}"
 #NEWNAME=$(ls "$i" | sed -e 's/txt/php/")
--- readme.md
+++ readme.md
@@ -10,8 +10,8 @@ friend.sh
 - append to file
 
 for.sh
-- still on process
-- have a problem
+- for statement
+- changing file's extension
 
 CR: http://www.tldp.org/LDP/Bash-Beginners-Guide/html/sect_08_02.html
 https://stackoverflow.com/questions/1224766/how-do-i-rename-the-extension-for-a-batch-of-files
